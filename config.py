f965a40dcd9101f11ba19883329222a49249d3a4
dreamer20 <gv4alex@gmail.com>
2022-11-15 17:02:53 +0300

    Add gunicorn to dependencies, change config params

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    DATABASE = 'mydb'
-    DATABASE_USER = 'postgres'
-    DATABASE_PASSWORD = 'peri54ri7end'
-    DATABASE_HOST = 'localhost'
+    DATABASE = 'test_db_ol7m'
+    DATABASE_USER = 'test_db_ol7m_user'
+    DATABASE_PASSWORD = 'FeJOb7TqR679zz1NnIdJ9jShL81aBV3j'
+    DATABASE_HOST = 'dpg-cdpp86mn6mppiad8oc0g-a'
     SECRET_KEY = os.urandom(12)
